d7762a6129a5fb1349e96cb6d71d8be152bcbd93
unknown <mf7753@GDD-21.main.ad.rit.edu>
2015-11-12 17:54:25 -0500

    Added take damage

--- NitrosIdea/Assets/scripts/PlayerController.cs
+++ NitrosIdea/Assets/scripts/PlayerController.cs
@@ -5,11 +5,13 @@ public class PlayerController: MonoBehaviour {
 
 	public GameObject player;
 	public float speed;
+	public float health;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
 		speed = 0.1f;
+		health = 100;
 	}
 	
 	// Update is called once per frame
@@ -21,5 +23,8 @@ public class PlayerController: MonoBehaviour {
 		if (Input.GetKey (KeyCode.A)) transform.position += (transform.right * -speed);
 		if (Input.GetKey (KeyCode.S)) transform.position += (transform.forward * -speed);
 		if (Input.GetKey (KeyCode.D)) transform.position += (transform.right * speed);
+	}
+	void TakeDamage(float damage){	
+		health -= damage;
 	}
 }
