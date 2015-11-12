d783c13fcfc7cd0d5db7939fc14b0f62473b2476
unknown <pmm9335@GDD-22.main.ad.rit.edu>
2015-11-12 15:23:16 -0500

    Added blank tile art of various colors, added a nonfunctional script for tile material picker

---
+++ NitrosIdea/Assets/scripts/tileMaterialPicker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tileMaterialPicker : MonoBehaviour {
+
+
+    [System.Serializable]
+    class Materials
+    {
+        Material tile00;
+        Material tile01;
+        Material tile02;
+        Material tile03;
+        Material tile04;
+        Material tile05;
+        Material tile06;
+        Material tile07;
+        Material tile08;
+        Material tile09;
+        Material tile10;
+        Material tile11;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
