0dcb8404fd4a75a9ad558729519bcb21370d61de
unknown <dwe5866@GDD-24.main.ad.rit.edu>
2015-11-14 21:22:17 -0500

    added projectile to screen, added projectile script, added projectile prefab

---
+++ NitrosIdea/Assets/scripts/plusProjectile.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plusProjectile : MonoBehaviour {
+
+    public float speed = 10;
+    public string dir;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (dir == "up") this.transform.position += speed * transform.forward;
+        else if (dir == "down") this.transform.position += speed * transform.forward * -1;
+        else if (dir == "left") this.transform.position += speed * transform.right * -1;
+        else if (dir == "right") this.transform.position += speed * transform.right;
+
+	
+	}
+
+    public void Up()
+    {
+        dir = "up";
+    }
+
+    public void Down()
+    {
+        dir = "down";
+    }
+
+    public void Left()
+    {
+        dir = "left";
+    }
+
+    public void Right()
+    {
+        dir = "right";
+    }
+}
