6f2a6b12479a082f6e925059fc327a73a2e8cbc2
unknown <pmm9335@GDD-27.main.ad.rit.edu>
2015-11-19 15:51:07 -0500

    Fixed merge issues for real between me and Wes

--- NitrosIdea/Assets/scripts/PlayerController.cs
+++ NitrosIdea/Assets/scripts/PlayerController.cs
@@ -1,34 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController: MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
-	public GameObject player;
-	public float speed;
-	public float health;
-	public GameObject floor;
+    [SerializeField]
+    GameObject player;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float health;
+    [SerializeField]
+    GameObject floor;
+    [SerializeField]
+    float tileUseTime;
 
-	// Use this for initialization
-	void Start () {
+    public Tile currTile;
+    bool usingTile;
+
+
+    // Use this for initialization
+    void Start()
+    {
         floor = GameObject.FindGameObjectWithTag("Floor");
-		player = GameObject.FindGameObjectWithTag("Player");
-		speed = 0.1f;
-		health = 100f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		Move ();
-	}
-	void Move() {
-		if (Input.GetKey (KeyCode.W)) transform.position += (transform.forward * speed);
-		if (Input.GetKey (KeyCode.A)) transform.position += (transform.right * -speed);
-		if (Input.GetKey (KeyCode.S)) transform.position += (transform.forward * -speed);
-		if (Input.GetKey (KeyCode.D)) transform.position += (transform.right * speed);
-	}
-	void TakeDamage(float damage){	
-		health -= damage;
-	}
-	void UseTile(){
-	}
+        player = GameObject.FindGameObjectWithTag("Player");
+        speed = 0.1f;
+        health = 100f;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (transform.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += (transform.right * -speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += (transform.forward * -speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += (transform.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && currTile)
+        {
+            usingTile = true;
+            StartCoroutine("UseTile");
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            usingTile = false;
+        }
+    }
+
+    void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+
+    IEnumerator UseTile()
+    {
+        yield return new WaitForSeconds(tileUseTime);
+        if (!usingTile)
+        {
+            yield break;
+        }
+
+        if (currTile)
+        {
+            currTile.FireAttack();
+        }
+    }
 }
