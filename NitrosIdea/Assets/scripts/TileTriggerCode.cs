0c2abafca349f06c7101113520f971494832e5d7
unknown <wsw3877@GDD-23.main.ad.rit.edu>
2015-11-19 17:43:50 -0500

    Added the tile script to the tiles and the triggerzone. Made the triggerzone now give the player the referance to the tile

---
+++ NitrosIdea/Assets/scripts/TileTriggerCode.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileTriggerCode : MonoBehaviour 
+{
+    Tile parentTile;
+
+    void Start()
+    {
+        parentTile = transform.parent.gameObject.GetComponent<Tile>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.GetComponent<PlayerController>().currTile = parentTile;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        other.gameObject.GetComponent<PlayerController>().currTile = null;
+    }
+}
