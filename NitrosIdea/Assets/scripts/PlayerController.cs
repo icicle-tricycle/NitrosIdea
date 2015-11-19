ca67400fcfc34c2bc39cd360e04a41567bd43627
unknown <wsw3877@GDD-23.main.ad.rit.edu>
2015-11-19 15:42:07 -0500

    Made the tile attack method public. The player can now press E to use a tile

--- NitrosIdea/Assets/scripts/PlayerController.cs
+++ NitrosIdea/Assets/scripts/PlayerController.cs
@@ -3,13 +3,19 @@ using System.Collections;
 
 public class PlayerController: MonoBehaviour {
 
-	public GameObject player;
-	public float speed;
-	public float health;
-	public GameObject[,] tiles;
+	[SerializeField] GameObject player;
+	[SerializeField] float speed;
+	[SerializeField] float health;
+	[SerializeField] GameObject[,] tiles;
+    [SerializeField] float tileUseTime;
+
+    public Tile currTile;
+    bool usingTile;
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		tiles = GameObject.FindGameObjectWithTag ("Floor").GetComponent <TileArray>().GetGrid;
 		player = GameObject.FindGameObjectWithTag("Player");
 		speed = 0.1f;
@@ -17,18 +23,61 @@ public class PlayerController: MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () 
+    {
 		Move ();
 	}
-	void Move() {
-		if (Input.GetKey (KeyCode.W)) transform.position += (transform.forward * speed);
-		if (Input.GetKey (KeyCode.A)) transform.position += (transform.right * -speed);
-		if (Input.GetKey (KeyCode.S)) transform.position += (transform.forward * -speed);
-		if (Input.GetKey (KeyCode.D)) transform.position += (transform.right * speed);
+
+	void Move() 
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += (transform.forward * speed);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += (transform.right * -speed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += (transform.forward * -speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += (transform.right * speed);
+        }
+        if (Input.GetKeyDown(KeyCode.E) && currTile)
+        {
+            usingTile = true;
+            StartCoroutine("UseTile");
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            usingTile = false;
+        }
 	}
-	void TakeDamage(float damage){	
+
+	void TakeDamage(float damage)
+    {	
 		health -= damage;
 	}
-	void UseTile(){
+
+	void UseTile()
+    {
+
 	}
+
+    IEnumerator UseTile()
+    {
+        yield return new WaitForSeconds(tileUseTime);
+        if (!usingTile)
+        {
+            yield break;
+        }
+
+        if (currTile)
+        {
+            currTile.FireAttack();
+        }
+    }
 }
